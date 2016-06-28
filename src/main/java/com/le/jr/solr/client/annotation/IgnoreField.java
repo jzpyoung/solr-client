ec9de99e2632a9dcbd0e91ebc049723a9f07b25e
Zhipeng Jia(贾志鹏) <jiazhipeng@letv.com>
2016-06-28 18:35:00 +0800

    trim

--- src/main/java/com/le/jr/solr/client/annotation/IgnoreField.java
+++ src/main/java/com/le/jr/solr/client/annotation/IgnoreField.java
@@ -17,5 +17,4 @@ import java.lang.annotation.Target;
 public @interface IgnoreField {
 
     String name() default "";
-
 }
--- src/main/java/com/le/jr/solr/client/annotation/ScopeField.java
+++ src/main/java/com/le/jr/solr/client/annotation/ScopeField.java
@@ -30,5 +30,4 @@ public @interface ScopeField {
         ScopeFiledEnum() {
         }
     }
-
 }
